0dacdf1ce4e631a952855537cf0df7548d47d135
rygo6 <rygo6@me.com>
2019-11-23 21:50:25 -0800

    Fixes. Level UI. Pooling.

---
+++ Scripts/AddressablesExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace GeoTetra.GTPooling
+{
+    public static class AddressablesExtensions
+    {
+        public static async Task<AsyncOperationHandle<GameObject>> PoolInstantiateAsync(this AssetReference reference)
+        {
+            return await AddressablesPool.InstantiateAsync(reference);
+        }
+        
+        public static async Task<T> PoolInstantiateAsync<T>(this AssetReference reference) where T : Object
+        {
+            #if UNITY_EDITOR
+            Debug.Log("PoolInstantiateAsync " + reference.editorAsset);
+            #endif
+            AsyncOperationHandle<GameObject> handle = await PoolInstantiateAsync(reference);
+            return handle.Result.GetComponent<T>();
+        }
+    }
+}
---
+++ Scripts/AddressablesPool.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace GeoTetra.GTPooling
+{
+    public class AddressablesPool
+    {
+        private static Dictionary<int, Object> _objectsInUse = new Dictionary<int, Object>();
+        private static OrderedDictionary _releasedObjects = new OrderedDictionary();
+
+        public static async Task<AsyncOperationHandle<GameObject>> InstantiateAsync(string key)
+        {
+            AsyncOperationHandle<GameObject> handle = Addressables.InstantiateAsync(key, trackHandle: true);
+            await handle.Task;
+            return handle;
+        }
+
+        public static async Task<AsyncOperationHandle<GameObject>> InstantiateAsync(AssetReference reference)
+        {
+            AsyncOperationHandle<GameObject> handle = await InstantiateAsync(reference.RuntimeKey as string);
+            return handle;
+        }
+        
+//        public static async Task<T> PoolInstantiateAsync<T>(this AssetReference reference) where T : Object
+    }
+}
+
