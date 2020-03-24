23bbcece7ea3e7e0d3c8937fdf29c61cddbc20a0
rygo6 <rygo6@me.com>
2020-03-24 00:18:56 -0700

    Fixed bunch of namespaces. Renamed folders. Added new shader for just coors. Setup Analytics. Setup Ads. Improved Gizmo.

--- Scripts/ServiceReference.cs
+++ Scripts/ServiceReference.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 namespace GeoTetra.GTPooling
 {
-    [Serializable]
+    [System.Serializable]
     public class ServiceReference : AssetReferenceT<ScriptableObject>
     {
         public ServiceReference(string guid) : base(guid) { }        
@@ -24,6 +23,31 @@ namespace GeoTetra.GTPooling
         {
             if (_service == null) LoadServiceFromPool();
             return (T) _service;
+        }
+    }
+    
+    [System.Serializable]
+    public class ServiceReferenceT<ServiceType> : AssetReferenceT<ServiceType> where ServiceType : ScriptableObject
+    {
+        public ServiceReferenceT(string guid) : base(guid) { }
+
+        private ServiceType _service;
+
+        public ServiceType Service
+        {
+            get
+            {
+                if (_service == null) LoadServiceFromPool();
+                return _service;
+            }
+        }
+
+        private void LoadServiceFromPool()
+        {
+            //If the Asset is not explicitly set, then it will try to load one that is named the same as the type.
+            _service = string.IsNullOrEmpty(AssetGUID) ? 
+                AddressableServicesPool.GlobalPool.PrePooledPopulate<ServiceType>() : 
+                AddressableServicesPool.GlobalPool.PrePooledPopulate<ServiceType>(this);
         }
     }
 }
